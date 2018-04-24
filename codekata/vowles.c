bbd7f4aed4d9ef897434b603212c8c227588910e
gokul003 <38651191+gokul003@users.noreply.github.com>
2018-04-24 15:02:54 +0530

    Create vowles.c

---
+++ codekata/vowles.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+char a;
+printf("enter the numeber");
+scanf("%d",a);
+
+if(ch='A'||ch='a'||ch='E'||ch='e'||ch='I'||ch='i'||ch='0'||ch='0'||ch='U'||ch='u')
+{
+printf ("vowles");
+}
+else
+{
+printf ("consonent");
+}
+return 0;
+}
