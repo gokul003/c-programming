858927dcbc0f1e747b7ebb8d05b957a9ca382c38
gokul003 <38651191+gokul003@users.noreply.github.com>
2018-07-07 06:58:27 +0000

    Create oe.c

---
+++ codekata/oe.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+int n;
+scanf("%f",&n);
+printf("enter the number:")
+  if(1<=n<=10000)
+   {
+      else if(n%2==0)
+        {
+            printf("entered number is even");
+         }
+       else
+        {
+          printf("entered number is odd")
+        }
+      }
+  else
+  {
+    printf( "invalid")
+  }
+
+
+
+
+
+}
