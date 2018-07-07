afe4ffdf18a767218e31781e03586adc19c3e10e
gokul003 <38651191+gokul003@users.noreply.github.com>
2018-07-07 07:36:39 +0000

    Create alphabet.c

---
+++ codekata/alphabet.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+char a;
+printf("enter the alphabet");
+scanf("%c",&a);
+  if(a>='a' && a<='z')
+  {
+      printf("it is an alphabet");
+   }
+  else
+   {
+      printf("it is not an alphabet");
+   }
+
+}
