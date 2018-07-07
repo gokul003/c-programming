01e610c2c02151af80b921df4a8b2bd2ac065235
gokul003 <38651191+gokul003@users.noreply.github.com>
2018-07-07 07:23:59 +0000

    Create vowel.c

---
+++ codekata/vowel.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+char a;
+printf("enter the alphabet\n");
+scanf("%c",&a);
+
+switch(a)
+{	
+  case 'a':
+	case 'e':
+	case 'i':
+	case 'o':
+	case 'u':
+		printf("it is a vowel");	
+		break;
+	default:
+	        printf("it is a constant");
+}
+}	
+	
